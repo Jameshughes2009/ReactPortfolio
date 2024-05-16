cb36005c4253a34c30fb7f4d4a8fb2fa52b64e2e
James Hughes <jameshughes2009@hotmail.com>
2024-05-16 10:33:06 -0400

    working on pages

---
+++ src/components/Projects.jsx
@@ -0,0 +1,7 @@
+import React, {useState} from "react";
+import { FaGithubSquare } from "react-icons/fa";
+import { GrDeploy} from "react-icons/gr"
+
+export default function Projects() {
+    
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import About from "./pages/About.jsx";
+
 
 const router = createBrowserRouter([
     {
