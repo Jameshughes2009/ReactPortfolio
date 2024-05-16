544e42b9e3fcbcd12f446c10d6581020f5a631b1
James Hughes <jameshughes2009@hotmail.com>
2024-05-16 10:05:14 -0400

    building Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+
+function Navbar() {
+    const currentPage = useLocation().pathname;
+
+    useEffect(() => 
+        const navbarToggler = document.querySelector(".navbar-toggler");
+
+        const handleNavbarToggle = () => {}
+)
+}
+
+export default Navbar
