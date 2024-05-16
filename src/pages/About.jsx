4aa01963d42630db9707996a888ccf6e8ebc89fa
James Hughes <jameshughes2009@hotmail.com>
2024-05-16 10:44:45 -0400

    About pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,17 @@
 export default function About() {
-    return
+    return (
+        <div className="container">
+            <div className="text-center">
+                <h1 className="dev-name"> James Hughes </h1>
+                <h4 className="dev-title"> UofT Full Stack WebDev Module 20</h4>
+            </div>
+            <div className="row">
+                <div className="col-md-4 text center">
+                    <img></img>
+                    <ul className="list-unstyled mt-3"></ul>
+                </div>
+            </div>
+        </div>
+    )
 }
+// adding info for images later
