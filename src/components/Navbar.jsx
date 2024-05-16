2c60e94b4d7f1112bef1fa55251901e1ba593ad5
James Hughes <jameshughes2009@hotmail.com>
2024-05-16 10:13:43 -0400

    more components addition for Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,25 @@ import { Link, useLocation } from "react-router-dom";
 function Navbar() {
     const currentPage = useLocation().pathname;
 
-    useEffect(() => 
+    useEffect(() => {
         const navbarToggler = document.querySelector(".navbar-toggler");
 
-        const handleNavbarToggle = () => {}
-)
+        const handleNavbarToggle = () => {
+            const targetId = navbarToggler.getAttribute("data-target");
+            const targetElement = document.getElementById(targetId.slice(1));
+
+            if (targetElement) {
+                const expanded = targetElement.classList.contains("show");
+                targetElement.classList.toggle("show", !expanded);
+            }
+        };
+
+        navbarToggler.addEventListener("click", handleNavbarToggle);
+
+        return() => {
+            navbarToggler.removeEventListener("click", handleNavbarToggle);
+        };
+}, []);
 }
 
 export default Navbar
