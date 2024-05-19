a21b910b34d7e632bf898d20cf1416ad540a2863
James Hughes <jameshughes2009@hotmail.com>
2024-05-19 15:29:43 -0400

    more resume functions

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -13,5 +13,44 @@ import { FaBootstrap } from "react-icons/fa";
 
 export default function Resume() {
     const fileUrl = 
-        "https://github.com/Jameshughes2009/ReactPortfolio/blob/main/src/assets/files/james%20Hughes%20resume%20(edited).docx"
+        "https://github.com/Jameshughes2009/ReactPortfolio/blob/main/src/assets/files/jamesResume.docx"
+    
+    return (
+        <div id="skills-container" className="container">
+            <div className="row">
+                <div className="col-md-6">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Frontend Setup</h5>
+                            <p className="card-text">
+                                <ul>
+                                    <li>
+                                        <FaHtml5 size={30} />
+                                        HTML
+                                    </li>
+                                    <li>
+                                        <FaCss3Alt size={30} />
+                                        Css
+                                    </li>
+                                    <li>
+                                        <FaBootstrap size={30} />
+                                        Bootstrap
+                                    </li>
+                                    <li>
+                                        <IoLogoJavascript size={23} />
+                                        Java + <DiJqueryLogo size={30}/>
+                                        JQuery
+                                    </li>
+                                    <li>
+                                        <FaReact size={27} />
+                                        React
+                                    </li>
+                                </ul>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
