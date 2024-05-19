502639431a5ae9414b3c4f2d7fd57708622ca7d0
James Hughes <jameshughes2009@hotmail.com>
2024-05-19 10:08:42 -0400

    more working

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,17 @@
 import React, {useState} from "react";
 import { FaGithubSquare } from "react-icons/fa";
 import { GrDeploy} from "react-icons/gr"
+import { groupPojects } from "../utils/groupProjects";
+import { moduleList } from "../utils/moduleList";
+import { schoolRepo } from "../utils/schoolRepo";
 
 export default function Projects() {
-    
+    const [projects] = useState(groupPojects);
+    const [school] = useState(schoolRepo);
+    const [module] = useState(moduleList);
+
+    return (
+        <>
+        </>
+    )
 }
--- src/utils/schoolRepo.js
+++ src/utils/schoolRepo.js
@@ -3,5 +3,5 @@ export const schoolRepo = [
         projectName: "All Class Notes and Material From UofT",
         projectUrl: "https://github.com/Jameshughes2009/Firstday",
         projectDescription: " This contain all the info I have be taught during the Bootcamp"
-    }
+    },
 ];
