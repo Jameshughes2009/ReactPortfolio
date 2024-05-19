101b7fb71898f282623dbbdff0f9c397e5a39811
James Hughes <jameshughes2009@hotmail.com>
2024-05-19 12:47:34 -0400

    starting the contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,19 @@
 import React, {useState} from "react";
 
 export default function ContactForm() {
-    
+    const [formData, setFormData] = useState({
+        name:"",
+        email:"",
+        message:""
+    });
+
+    const [validationErrors, setValidationErrors] = useState({
+        name: false,
+        email: false,
+        message: false
+    });
+
+    const handleChange = (e) => {
+        
+    }
 }
