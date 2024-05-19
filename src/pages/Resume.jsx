cc8dffb654d15e45f5e29566c76433d4688199eb
James Hughes <jameshughes2009@hotmail.com>
2024-05-19 14:29:59 -0400

    working on resume section

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,3 +1,16 @@
+import React from "react";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { DiJqueryLogo } from "react-icons/di";
+import { FaReact } from "react-icons/fa";
+import { GrMysql } from "react-icons/gr";
+import { SiSequelize } from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa";
+import { SiExpress } from "react-icons/si";
+import { SiMongodb } from "react-icons/si";
+import { FaBootstrap } from "react-icons/fa";
+
 export default function Resume() {
     
 }
