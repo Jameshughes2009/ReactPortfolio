da477842cfbed6055d915560d737489b93acf8e8
James Hughes <jameshughes2009@hotmail.com>
2024-05-19 11:17:56 -0400

    fixing project linking

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,37 +10,23 @@ export default function Projects() {
     const [school] = useState(schoolRepo);
     const [module] = useState(moduleList);
 
-    return (
+
+    return(
         <>
-            <div id = "team-projects-container" className="container">
-                <h2 className="project-tittle">All Work Done With Other UofT Students</h2>
+            <div id="team-projects-container" className="container">
+                <h2 className="project-title">Team Group work at UofT</h2>
                 <div className="row">
-                    {projects.map((project, index) => (
+                    {projects.map ((project, index) => (
                         <div key={index} className="col-md-4 mb-4">
                             <div className="card">
-                                <div className="card-body">
-                                    <h5 className="card-title">{project.projectName}</h5>
-                                    <p className="card-text">
-                                        Module: {project.projectDescription}
-                                    </p>
-                                    <a
-                                        href={project.projectGithub}
-                                        target="_blank"
-                                        className="card-text"
-                                        id="card-anchor"
-                                    >
-                                        <FaGithubSquare size={25} className="github-icon"/>
-                                    </a>
-                                    <a
-                                        href={project.projectUrl}
-                                        target="_blank"
-                                        className="card-text"
-                                        id="card-anchor"
-                                    >
-                                        <GrDeploy size={20} className="deplop-icon" />
-                                        Deployed
-                                    </a>
-                                </div>
+                                <img
+                                    id="project-images"
+                                    src={project.projectName}
+                                    className="card-img-top"
+                                    alt={project.projectName}
+                                />
+                            </div>
+                        </div>
                     ))}
                 </div>
             </div>
