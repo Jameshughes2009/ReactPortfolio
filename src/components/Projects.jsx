273222bb16ab6892ed231d36ba05df57d485ff46
James Hughes <jameshughes2009@hotmail.com>
2024-05-19 12:03:46 -0400

    adding linked indivul project to page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,7 +13,7 @@ export default function Projects() {
 
     return(
         <>
-            <div id="team-projects-container" className="container">
+            <div id="deployed-apps-container" className="container">
                 <h2 className="project-title">Team Group work at UofT</h2>
                 <div className="row">
                     {projects.map ((project, index) => (
@@ -54,6 +54,51 @@ export default function Projects() {
                     ))}
                 </div>
             </div>
+
+            <div id="deployed-apps-container" className="container">
+                <h2 className="project-title">Individual assignments </h2>
+                <div className="row">
+                    {module.map((project, index) =>(
+                        <div key={index} className="col-md-4 mb-4">
+                            <div className="card">
+                                <img
+                                    id="project-images"
+                                    src={project.projectUrl}
+                                    className="card-img-top"
+                                    alt={project.projectName}
+                                />
+
+                                <div className="card-body">
+                                    <h5 className="card-title">{project.projectName}</h5>
+                                    <p className="card-text">
+                                        Module: {project.projectDescription}
+                                    </p>
+                                    <a
+                                        href={project.projectGithub}
+                                        target="_blank"
+                                        className="card-text"
+                                        id="card-anchor"
+                                    >
+                                        <FaGithubSquare size={30} className="github-icon" />
+                                        github Repo
+                                    </a>
+                                    <a
+                                        href={project.projectUrl}
+                                        target="_blank"
+                                        className="card-text"
+                                        id="card-anchor"
+                                    >
+                                        <GrDeploy size={25} className="deploy-icon" />
+                                        App Link
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            
         </>
     )
 }
