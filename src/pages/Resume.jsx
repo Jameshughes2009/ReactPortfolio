0feeec19eb7a56087b216c2701e29f9957d4cb10
James Hughes <jameshughes2009@hotmail.com>
2024-05-19 15:32:15 -0400

    function edits

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -50,6 +50,8 @@ export default function Resume() {
                         </div>
                     </div>
                 </div>
+
+                
             </div>
         </div>
     )
