c7f4c024122a11efa914b16c692acc4ddf21921e
James Hughes <jameshughes2009@hotmail.com>
2024-05-21 12:10:53 -0400

    more info

--- src/utils/schoolRepo.js
+++ src/utils/schoolRepo.js
@@ -4,4 +4,10 @@ export const schoolRepo = [
         projectUrl: "https://github.com/Jameshughes2009/Firstday",
         projectDescription: " This contain all the info I have be taught during the Bootcamp"
     },
+    {
+        projectName: "UofT Student Portal",
+        projectUrl: "https://bootcampspot.instructure.com/courses/4895",
+        projectDescription: " All Class info given to us by UofT"
+    },
+    
 ];
