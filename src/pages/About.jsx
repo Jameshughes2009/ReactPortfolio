0c5b1429bb1f52e0d8e42e88fc022c73058bd24e
James Hughes <jameshughes2009@hotmail.com>
2024-05-21 09:39:03 -0400

    more info

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,12 +3,13 @@ export default function About() {
         <div className="container">
             <div className="text-center">
                 <h1 className="dev-name"> James Hughes </h1>
-                <h4 className="dev-title"> UofT Full Stack WebDev Module 20</h4>
+                <h5 className="dev-title"> UofT Full Stack WebDev Module 20</h5>
             </div>
             <div className="row">
-                <div className="col-md-4 text center">
+                <div className="col-md-3 text center">
                     <img>
                     </img>
+                    <h4>Current Skills List</h4>
                     <ul className="list-unstyled mt-3">
                         <li>
                             <strong>Hardworking</strong> 
@@ -21,14 +22,14 @@ export default function About() {
                         </li>
                     </ul>
                 </div>
-                <div className="col-md-8">
+                <div className="col-md-7">
                     <section className="right-container text-left">
                         <p> 
-                        Currenlty working full time at Microbix Biosystems Inc and doing this class. It has been a great expreice to be part of this and have the backing of my employeer. The bootcamp I will say needs so work as i belive there is not enough time even with the partime camp to full learn all content!
+                            Currenlty working full time at Microbix Biosystems Inc and doing this class. It has been a great expreice to be part of this and have the backing of my employeer. The bootcamp I will say needs so work as i belive there is not enough time even with the partime camp to full learn all content!
                         </p>
                         <br/>
                         <p> 
-                            I hope you'll expore my GitHub acciunt to get an insight it all the work that has been done durring this 6 month course. My new skill learned from this full stack bootcamp include, HTML/CSS , Java, JQuery, Postman, Express.js, OOP, ORM, MVC and many other skills thoughout the course.
+                            I hope you'll expore my GitHub acciunt to get an insight it all the work that has been done durring this 6 month course. My new skill learned from this full stack bootcamp include, HTML/CSS , Java, JQuery, Postman, Express.js, OOP, ORM, MVC and many other skills thoughout the course. Please find the links to both my github account and linkedin profile in the bottom left of all pages!
                         </p>
                     </section>
                 </div>
